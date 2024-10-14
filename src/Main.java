b7b7d525436f03c71321aef6676171909da06455
StenographerSure <glibockiy@inbox.ru>
2024-10-14 22:17:57 +0400

    HOMEWORK 2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         if (clientOS == 0){
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
-        else if(clientOS == 1){
+        else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
@@ -19,26 +19,25 @@ public class Main {
             if (clientOS == 0) {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
-            else if (clientOS == 1) {
+            else{
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
-        else {
-            if (clientOS == 0) {
+        else if (clientOS == 0) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             }
-            else if (clientOS == 1) {
+            else {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
 
-        }
+
 
         //задача 3
         System.out.println("Задача 3");
 
         int year = 2021;
 
-        if ( year%4 == 0 && year%100 != 0 && year > 1584 ){
+        if ( year%4 == 0 && (year%100 != 0 || year%400 == 0) && year > 1584 ){
             System.out.println(year + " год является високосным");
         }
         else {
@@ -67,26 +66,44 @@ public class Main {
         System.out.println("Задача 5");
 
         int monthNumber = 12;
-        if (monthNumber <= 12) {
+        if (monthNumber > 12) {
+            System.out.println("Месяц задан некорректно");
+        }
+        else {
             switch (monthNumber) {
                 case 1:
+                    System.out.println("Зима");
+                    break;
                 case 2:
                     System.out.println("Зима");
                     break;
                 case 3:
+                    System.out.println("Весна");
+                    break;
                 case 4:
+                    System.out.println("Весна");
+                    break;
                 case 5:
                     System.out.println("Весна");
                     break;
                 case 6:
+                    System.out.println("Лето");
+                    break;
                 case 7:
+                    System.out.println("Лето");
+                    break;
                 case 8:
                     System.out.println("Лето");
                     break;
                 case 9:
+                    System.out.println("Осень");
+                    break;
                 case 10:
+                    System.out.println("Осень");
+                    break;
                 case 11:
                     System.out.println("Осень");
+                    break;
                 case 12:
                     System.out.println("Зима");
                     break;
