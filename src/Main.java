74e450816538c8094ad4df2719cea40d885cf00c
StenographerSure <glibockiy@inbox.ru>
2024-10-15 19:09:14 +0400

    HOMEWORK 2 rework

--- src/Main.java
+++ src/Main.java
@@ -66,48 +66,32 @@ public class Main {
         System.out.println("Задача 5");
 
         int monthNumber = 12;
-        if (monthNumber > 12) {
-            System.out.println("Месяц задан некорректно");
-        }
-        else {
-            switch (monthNumber) {
+        switch (monthNumber) {
                 case 1:
-                    System.out.println("Зима");
-                    break;
                 case 2:
                     System.out.println("Зима");
                     break;
                 case 3:
-                    System.out.println("Весна");
-                    break;
                 case 4:
-                    System.out.println("Весна");
-                    break;
                 case 5:
                     System.out.println("Весна");
                     break;
                 case 6:
-                    System.out.println("Лето");
-                    break;
                 case 7:
-                    System.out.println("Лето");
-                    break;
                 case 8:
                     System.out.println("Лето");
                     break;
                 case 9:
-                    System.out.println("Осень");
-                    break;
                 case 10:
-                    System.out.println("Осень");
-                    break;
                 case 11:
                     System.out.println("Осень");
                     break;
                 case 12:
                     System.out.println("Зима");
                     break;
-            }
+                default:
+                    System.out.println("Месяц задан некорректно");
         }
+
     }
 }
