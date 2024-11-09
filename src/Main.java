27b19b659b7cc8a6549ad506290e2ed9ac0aa30d
StenographerSure <glibockiy@inbox.ru>
2024-11-09 14:36:55 +0400

    HOMEWORK methods

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задача 1
+        System.out.println("Задача 1");
 
+        checkLeapYear(2021);
 
+        //задача 2
+        System.out.println("Задача 2");
 
     }
-    public static void checkLeapYear() {
-
+    public static void checkLeapYear(int year) {
+         if ( year%4 == 0 && (year%100 != 0 || year%400 == 0) && year > 1584 ){
+            System.out.println(year + " год — високосный год");
+        }
+        else {
+            System.out.println(year + " год — невисокосный год");
+        }
     }
 }
