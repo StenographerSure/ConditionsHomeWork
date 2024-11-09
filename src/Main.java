57f8f3a7c0f733f6c4ef08304600d90ac650c695
StenographerSure <glibockiy@inbox.ru>
2024-11-09 14:48:16 +0400

    HOMEWORK methods

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         //задача 1
@@ -8,7 +11,10 @@ public class Main {
         //задача 2
         System.out.println("Задача 2");
 
+        checkCompatibility(1, 2023);
+
     }
+
     public static void checkLeapYear(int year) {
          if ( year%4 == 0 && (year%100 != 0 || year%400 == 0) && year > 1584 ){
             System.out.println(year + " год — високосный год");
@@ -17,4 +23,19 @@ public class Main {
             System.out.println(year + " год — невисокосный год");
         }
     }
+    public static void checkCompatibility(int clientOS, int clientDeviceYear) {
+        if (clientDeviceYear >= LocalDate.now().getYear()) {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else if (clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+
 }
